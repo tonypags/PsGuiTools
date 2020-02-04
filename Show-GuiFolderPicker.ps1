ad22a29d54b5884d3adf093c30a40de1fa995e1c
Tony Pagliaro <tony@pagliaro.co>
2020-02-03 23:50:09 -0500

    Update and rename function

---
+++ Show-GuiFolderPicker.ps1
@@ -0,0 +1,36 @@
+Function Show-GuiFolderPicker {
+    <#
+    .SYNOPSIS
+    Returns s folder object from a GUI pop up window.
+    .DESCRIPTION
+    Displays a folder picker window for the user, allowing non-technical users to leverage an automated routine.
+    .PARAMETER Title
+    Message for the pop up window title bar
+    .PARAMETER InitialDirectory
+    Default path location of the picker window.
+    .PARAMETER outString
+    Limit the output to a string value
+    .EXAMPLE
+    Show-GuiFilePicker -Title 'Select a File' -Single
+    .NOTES
+    InitialDirectory ENUM: https://msdn.microsoft.com/en-us/library/system.environment.specialfolder(v=vs.110).aspx
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        $Title = 'Choose a Folder',
+
+        [Parameter()]
+        [ValidateSet('MyComputer','Recent','MyDocuments','Favorites','Desktop','Templates')]
+        $InitialDirectory = 'Recent'
+
+    )
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+    $OpenFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $OpenFolderDialog.RootFolder = $InitialDirectory
+    $OpenFolderDialog.Description = $Title 
+    $OpenFolderDialog.ShowDialog() | Out-Null
+    [string]$FullName = $OpenFolderDialog.SelectedPath
+    if ($outString) {$FullName} else {Get-Item $FullName}
+}
+
