84ad85c808f81657e85be9836541e8dcd381d59b
Tony Pagliaro <tony@pagliaro.co>
2020-02-03 23:38:15 -0500

    Update and rename Invoke-GuiFilePicker.ps1 to Show-GuiFilePicker.ps1

--- Show-GuiFilePicker.ps1
+++ Show-GuiFilePicker.ps1
@@ -1,40 +1,55 @@
-﻿Function Invoke-GuiFilePicker 
-{   
-    param(
+﻿function Show-GuiFilePicker {   
+    <#
+    .SYNOPSIS
+    Returns file objects from GUI pop up window.
+    .DESCRIPTION
+    Displays a file picker window for the user, allowing non-technical users to leverage an automated routine.
+    .PARAMETER Title
+    Message for the pop up window title bar
+    .PARAMETER InitialDirectory
+    Default path location of the picker window
+    .PARAMETER Extension
+    Filter the picker window by file type, supported: txt, csv, xml, pdf, xlsx, docx
+    .PARAMETER Single
+    Limit the picker window to a single selection
+    .PARAMETER outString
+    Limit the output to a string value(s)
+    .EXAMPLE
+    Show-GuiFilePicker -Title 'Select a File' -Single
+    #>
+    [CmdletBinding()]
+    param (
+        
         [string]$Title = "Select File(s)",
         [string]$InitialDirectory=
             "$($env:USERPROFILE)\Downloads",
         [string]$Extension, 
-        [switch]$Single=$false,
-        [switch]$outString=$false
+        [switch]$Single,
+        [switch]$outString
+        
     )
 
-    if(Test-Path $InitialDirectory){
+    if(Test-Path $InitialDirectory) {
 
-        if($Extension -match '\.|\*'){
+        if($Extension -match '\.|\*') {
             $Extension = $Extension -replace '\.' -replace '\*'
         }
 
-        $Filter = switch ($Extension)
-        {
+        
+        $Filter = switch ($Extension) {
+        
             'txt'  {"Text Files (*.txt) | *.txt" ; break }
             'csv'  {"CSV files (*.csv) | *.csv" ; break }
             'xml'  {"XML files (*.xml) | *.xml" ; break }
             'pdf'  {"PDF files (*.pdf) | *.pdf" ; break }
             'xlsx' {"Excel files (*.xlsx) | *.xlsx" ; break }
             'docx' {"Word files (*.docx) | *.docx" ; break }
-<#
-            # Catch-all (attempt)
-            "$($Extension)" 
-                   {"$($Extension.ToUpper()) files (*.$(
-                   $Extension.ToLower())) | *.$(
-                   $Extension.ToLower())" ; break }
-#>
+
             Default {"All Files (*.*) | *.*"}
         }
 
-        [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
-        
+
+        [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")        
         $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
 
         $OpenFileDialog.filter = $Filter
@@ -42,17 +57,19 @@
         $OpenFileDialog.RestoreDirectory = $true
         $OpenFileDialog.Title = $Title
         $OpenFileDialog.Multiselect = (!$Single)
-        #$OpenFileDialog.Topmost = $True  # not supported? causes error
         $OpenFileDialog.CheckFileExists = (!$outString)
         $OpenFileDialog.ShowDialog() | Out-Null
 
         [string[]]$FullName = $OpenFileDialog.Filenames
 
-        Write-Output (%{if ($outString){$FullName}else{Get-Item $FullName}})
+        if ($outString) {$FullName} else {Get-Item $FullName}
+
     }else{
+    
         Invoke-GuiPopupWarningOK `
             -MessageTitle "Path not Found!" `
             -MessageBody "The path $($InitialDirectory) cannot be found. "
+            
     }
 }
 
