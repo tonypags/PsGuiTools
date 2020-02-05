18b2c59c3cb6a85a433740605c74d5c9a093267b
Tony Pagliaro <tony@pagliaro.co>
2020-02-05 00:07:47 -0500

    Update and rename function.

--- Invoke-GuiTextInputDialog.ps1
+++
@@ -1,7 +0,0 @@
-﻿function Invoke-GuiTextInputDialog ([string]$Message="Enter some value:",[string]$Title="Input Text",[string]$Default=$null)
-{
-    Add-Type -AssemblyName Microsoft.VisualBasic
-    $inputText = [Microsoft.VisualBasic.Interaction]::InputBox($Message, $Title, $Default)
-    Write-Output $inputText
-}
-
---
+++ Show-GuiInputBox.ps1
@@ -0,0 +1,47 @@
+﻿function Show-GuiInputBox {
+
+    <#
+    .SYNOPSIS
+    Various window pop up configurations
+    .DESCRIPTION
+    Displays a popup window with which a non-technical user can interact, with options such as OK, Cancel, YesNo, Error, etc
+    .PARAMETER Title
+    Message for the pop up window title bar
+    .PARAMETER Message
+    Message or Description for list of items
+    #>
+    
+    param (
+        
+        [Parameter(Position=0)]
+        [string]
+        $Title = "Input Text",
+        
+        [Parameter(Position=1)]
+        [string]
+        $Message = "Enter a value:",
+        
+        [Parameter()]
+        [string]
+        $Default
+
+    )
+
+    Begin {
+
+        Try {
+            Add-Type -AssemblyName Microsoft.VisualBasic -ea Stop
+        } Catch {
+            throw 'Unable to load VB assembly.'
+        }
+
+    }
+
+    Process {}
+
+    End {
+        [Microsoft.VisualBasic.Interaction]::InputBox($Message, $Title, $Default)
+    }
+
+}
+
